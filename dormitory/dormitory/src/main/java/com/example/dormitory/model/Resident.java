666a86967b66f5bc9c44b79bd19bc55edd569168
JetnipitKaewsup <Jetnipit.Kaewsup@gmail.com>
2026-09-21 20:57:23 +0700

    chore : update Resident.java

--- dormitory/dormitory/src/main/java/com/example/dormitory/model/Resident.java
+++ dormitory/dormitory/src/main/java/com/example/dormitory/model/Resident.java
@@ -3,7 +3,28 @@ package com.example.dormitory.model;
 /**
  * Resident
  */
+@Entity
+@Table(name = "Resident")
 public class Resident {
+ // PK
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID residentId;
 
+    // FK
+    /*
+     * @ManyToOne
+     * 
+     * @JoinColumn(name = "dormitoryId")
+     * private Dormitory dormitory;
+     */
+
+    @ManyToOne
+    @JoinColumn(name = "roomNo")
+    private Room room;
+
+    private String firstName;
+    private String lastName;
+    private String phoneNo;
     
 }
