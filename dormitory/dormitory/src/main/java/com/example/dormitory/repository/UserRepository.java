fe27bf770b30641bcfc9e689d5d7a485ea3f8ab1
evalalalaeva <dgvgvjv@gmail.com>
2026-09-22 03:37:43 +0700

    feat: add UserRepository

---
+++ dormitory/dormitory/src/main/java/com/example/dormitory/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.example.dormitory.repository;
+
+import com.example.dormitory.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByUsername(String username);
+}
