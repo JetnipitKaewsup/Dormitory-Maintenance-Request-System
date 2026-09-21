e32f5baadb3a26a360311f4ce574767a81726710
evalalalaeva <dgvgvjv@gmail.com>
2026-09-22 02:19:38 +0700

    feat: add admin.java

--- dormitory/dormitory/src/main/java/com/example/dormitory/model/Admin.java
+++ dormitory/dormitory/src/main/java/com/example/dormitory/model/Admin.java
@@ -1,5 +1,40 @@
 package com.example.dormitory.model;
+
+import jakarta.persistence.*;
+import java.util.UUID;
+
+@Entity
+@Table(name = "admin")
 public class Admin {
-//test
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID adminId;
+
+    @OneToOne
+    @JoinColumn(name = "userId", nullable = false)
+    private User user;
+
+    public Admin() {
+    }
+
+    public Admin(User user) {
+        this.user = user;
+    }
+
+    public UUID getAdminId() {
+        return adminId;
+    }
+
+    public void setAdminId(UUID adminId) {
+        this.adminId = adminId;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
