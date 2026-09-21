d9d9038c7fac0e6c372b6e62fed3326c663370ff
JetnipitKaewsup <Jetnipit.Kaewsup@gmail.com>
2026-09-22 01:23:18 +0700

    chore : Update Building and Room Model

---
+++ dormitory/dormitory/src/main/java/com/example/dormitory/model/Building.java
@@ -0,0 +1,51 @@
+package com.example.dormitory.model;
+
+import jakarta.persistence.*;
+
+@Entity 
+@Table (name = "Building")
+public class Building {
+    // PK
+    @Id 
+    private int buildingNo;
+
+    // FK
+    /* 
+    @ManyToOne
+    @JoinColumn (name = "dormitoryId")
+    private Dormitory dormitory;
+*/
+    private String buildingName;
+    private int totalFloor;
+
+    public int getBuildingNo() {
+        return buildingNo;
+    }
+
+    public void setBuildingNo(int buildingNo) {
+        this.buildingNo = buildingNo;
+    }
+
+    public String getBuildingName() {
+        return buildingName;
+    }
+
+    public void setBuildingName(String buildingName) {
+        this.buildingName = buildingName;
+    }
+
+    public int getTotalFloor() {
+        return totalFloor;
+    }
+
+    public void setTotalFloor(int totalFloor) {
+        this.totalFloor = totalFloor;
+    }
+
+    public Building(int buildingNo, String buildingName, int totalFloor) {
+        this.buildingNo = buildingNo;
+        this.buildingName = buildingName;
+        this.totalFloor = totalFloor;
+    }
+    
+}
