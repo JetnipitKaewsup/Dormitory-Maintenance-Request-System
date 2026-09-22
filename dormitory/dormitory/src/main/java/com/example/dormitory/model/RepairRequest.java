952b58f547dde9aebde76d8a278e58ed350a657a
Suchada <pc231248@gmail.com>
2026-09-22 13:45:24 +0700

    Revert "refactor: update attributes name and constructor"
    This reverts commit 470871605f66af331eb8ccbc92c6bb4f93876f97.

--- dormitory/dormitory/src/main/java/com/example/dormitory/model/RepairRequest.java
+++ dormitory/dormitory/src/main/java/com/example/dormitory/model/RepairRequest.java
@@ -1,14 +1,11 @@
 package com.example.dormitory.model;
 
-import com.example.dormitory.Reporter;
-import com.example.dormitory.Admin;
-import com.example.dormitory.Room;
 import jakarta.persistence.*;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Entity
-@Table(name = "RepairRequest")
+@Table(name = "repair_request")
 public class RepairRequest {
 
     @Id
@@ -16,15 +13,15 @@ public class RepairRequest {
     private UUID repairRequestId;
 
     @ManyToOne(optional = false)
-    @JoinColumn(name = "reporterId", nullable = false)
+    @JoinColumn(name = "reporter_id", nullable = false)
     private Reporter reporter;
 
     @ManyToOne
-    @JoinColumn(name = "adminId")
+    @JoinColumn(name = "admin_id")
     private Admin admin;
 
     @ManyToOne(optional = false)
-    @JoinColumn(name = "roomNo", nullable = false)
+    @JoinColumn(name = "room_no", nullable = false)
     private Room room;
 
     @Column(nullable = false)
@@ -44,22 +41,6 @@ public class RepairRequest {
     private LocalDateTime endDateTime;
 
     public RepairRequest() {
-    }
-
-    public RepairRequest(UUID repairRequestId, Reporter reporter, Admin admin,
-                        Room room, String repairType, String status,
-                        String description, String remark,
-                        LocalDateTime startDateTime, LocalDateTime endDateTime) {
-        this.repairRequestId = repairRequestId;
-        this.reporter = reporter;
-        this.admin = admin;
-        this.room = room;
-        this.repairType = repairType;
-        this.status = status;
-        this.description = description;
-        this.remark = remark;
-        this.startDateTime = startDateTime;
-        this.endDateTime = endDateTime;
     }
 
     // Getter / Setter
--- dormitory/dormitory/src/main/java/com/example/dormitory/model/Technician.java
+++ dormitory/dormitory/src/main/java/com/example/dormitory/model/Technician.java
@@ -13,16 +13,12 @@ public class Technician {
     private String specialization;
 
     @OneToOne(optional = false)
-    @JoinColumn(name = "userId", nullable = false, unique = true)
+    @JoinColumn(name = "user_id", nullable = false, unique = true)
     private User user;
 
     public Technician() {
     }
 
-    public Technician(UUID technicianId, User user) {
-        this.technicianId = technicianId;
-        this.user = user;
-    }
     // Getter / Setter
 
     public UUID getTechnicianId() {
